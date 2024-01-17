29678275fa3a0c4f9e2ec8585293a74fb036a091
iLetoni <letoni1996@gmail.com>
2024-01-17 23:13:32 +0300

    menu and container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Box, Container } from '@mui/material';
+import NavBar from './layout/navbar/NavBar';
 
 function App(): JSX.Element {
-  return <div>Hello</div>;
+  return (
+    <>
+      <NavBar />
+      <Container maxWidth="lg" sx={{ p: 2, display: 'flex', justifyContent: 'center' }}>
+        Тут роутинг
+      </Container>
+    </>
+  );
 }
 
 export default App;
---
+++ client/src/pages/home/home.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Home(): JSX.Element {
+  return <div>Hello</div>;
+}
+
+export default Home;
