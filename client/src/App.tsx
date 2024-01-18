6f0f09f2d52f8c3bc9da05f47e0bb2c57c32a110
iLETONI <letoni1996@gmail.com>
2024-01-18 13:35:10 +0300

    fix body margin

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,10 +10,8 @@ function App(): JSX.Element {
         maxWidth="lg"
         sx={{
           p: 2,
-          height: '100vh',
           display: 'flex',
           justifyContent: 'center',
-          alignItems: 'center',
         }}
       >
         Тут роутинг, мак длина lg
