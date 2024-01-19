de8d169e3cd03148d9f6a19dca45e8279f2a371c
EgorSmelov <smelov909@mail.ru>
2024-01-19 10:47:11 +0300

    footercheck

--- client/src/layout/footer/Footer.tsx
+++ client/src/layout/footer/Footer.tsx
@@ -1,6 +1,5 @@
 import { AppBar, Box, Toolbar, Typography } from '@mui/material';
 import React from 'react';
-import CopyrightIcon from '@mui/icons-material/Copyright';
 
 export default function Footer(): JSX.Element {
   return (
