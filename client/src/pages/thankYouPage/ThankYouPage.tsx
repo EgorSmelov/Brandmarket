9a06d16d2e7eaf3cefedfc890e312e65918e4187
iLETONI <letoni1996@gmail.com>
2024-01-25 18:18:59 +0300

    merge

--- client/src/pages/thankYouPage/ThankYouPage.tsx
+++ client/src/pages/thankYouPage/ThankYouPage.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 export default function ThankYouPage(): JSX.Element {
   return (
     <Typography component="h1" variant="h5">
-      Спасибо! Ваш заказ оформлен!
+      Спасибо! Ваш заказ оформлен! Чек отправлен на почту
     </Typography>
   );
 }
